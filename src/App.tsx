092d54866d4a66d5e9b270538b853f6a204d8786
Long Quoc Nguyen <longquoc47krb@gmail.com>
2023-01-06 14:51:27 +0700

    animate picture

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import "./App.css";
 import "./styles/global.scss";
 import Title from "./components/Title";
 import ChemicalElement from "./components/ChemicalElement";
-import { motion } from "framer-motion";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <Title>
           Welcome to the{" "}
           <ChemicalElement symbol="Br" atomicBlock={35} fontSize="4xl" />
-          eaking <ChemicalElement symbol="b" atomicBlock={56} fontSize="4xl" />
-          ad
+          eaking <ChemicalElement symbol="ba" atomicBlock={56} fontSize="4xl" />
+          d
         </Title>
-        <img src={introPicture} className="intro-picture" alt="intro" />
-      </header>
+        <motion.img
+          src={introPicture}
+          className="intro-picture"
+          alt="intro"
+          initial={{ x: 0, y: 200 }}
+          animate={{ x: -300, y: 0 }}
+          transition={{ ease: "easeOut", duration: 4 }}
+        />
+      </div>
     </div>
   );
 }
